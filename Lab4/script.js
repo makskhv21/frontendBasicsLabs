7476ed38aceb8cf5b1964315aab97ca8c751a9d4
repo <khvits21@gmail.com>
2024-10-03 12:09:36 +0300

    Add Laba4

---
+++ Lab4/script.js
@@ -0,0 +1,60 @@
+const photoContainer = document.getElementById("photo-container"); 
+const photo = document.getElementById("photoLviv");
+
+const btnAdd = document.getElementById('btnAdd'); 
+const btnDelete = document.getElementById('btnDelete');
+const btnIncrease = document.getElementById('btnIncrease');
+const btnReduce = document.getElementById('btnReduce');
+
+const initialWidth = photo.clientWidth;
+const maxWidth = initialWidth * 3; 
+const minWidth = initialWidth / 3; 
+
+// Function to update image width
+const updateImageWidth = (factor) => {
+    const lastImage = photoContainer.lastElementChild;
+    if (lastImage) {
+        const newWidth = lastImage.clientWidth * factor;
+        if ((factor > 1 && newWidth <= maxWidth) || (factor < 1 && newWidth >= minWidth)) {
+            lastImage.style.width = newWidth + 'px';
+        }
+    }
+};
+
+btnAdd.addEventListener("click", () => {
+    photoContainer.appendChild(photo.cloneNode(true));
+});
+
+btnDelete.addEventListener("click", () => {
+    const lastImage = photoContainer.lastElementChild;
+    if (lastImage) lastImage.remove();
+});
+
+btnIncrease.addEventListener("click", () => updateImageWidth(1.2));
+btnReduce.addEventListener("click", () => updateImageWidth(0.75));
+
+// Option number
+const n = 66;
+const elementIndex = (n % 10) + 1;
+
+// Select elements
+const firstElement = document.getElementById(elementIndex.toString());
+const secondElement = document.querySelector(`li[id="${elementIndex + 1}"]`); 
+
+// Change colors when clicked
+let clicks = [true, true];
+
+const handleClick = (element, index) => {
+    const textElement = element.querySelector('span');
+    if (clicks[index]) {
+        element.style.backgroundColor = '#001f3f'; 
+        textElement.style.color = '#f8f9fa'; 
+    } else {
+        element.style.backgroundColor = 'yellow'; 
+        textElement.style.color = '#6A1B9A'; 
+    }
+    clicks[index] = !clicks[index];
+};
+
+firstElement.addEventListener('click', () => handleClick(firstElement, 0));
+secondElement.addEventListener('click', () => handleClick(secondElement, 1));
